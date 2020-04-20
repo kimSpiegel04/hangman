4ac339ad3fd310e1067f0f96697ec0dc65c044c3
kimSpiegel04 <k.spiegel72@gmail.com>
2020-04-20 15:29:28 -0400

    broke game into functions, user can't input numbers or multiple letters at once

--- script.py
+++ script.py
@@ -1,80 +1,87 @@
 import random
 
-lines = open('./words.txt').readlines()
-line = lines[0]
-
-words = line.split(';')
-comp_word = random.choice(words)
-
-print(comp_word)
-
-letters = list(comp_word)
-# print(letters)
-
-game_array = []
-
-for char in letters:
-    if char.isalpha():
-        game_array.append('_')
-    else: 
-        game_array.append(char)
-
-#string of game_array
-replace_str = ''.join([str(char) for char in game_array])
-
-# this display keeps it pretty for the game
-game_display = ' '.join(replace_str)
-print(game_display)
-
-guesses = []
-
+# check to see if guess is in the string (allows for multiple matches)
 def isGuessCorrect(s, ch):
     return [i for i, letter in enumerate(s) if letter == ch]
 
-count = 7
+#check to see if game has been won
+def isGameWon(a):
+    if '_' not in a:
+        return('Congrats! You won the game!')
 
-while count > 0:
-
-    print('\nGuess a letter! You have', count, 'lives left.')
-    x = input().upper()
-    guess = isGuessCorrect(comp_word.upper(), x)
-
-    if x in guesses:
-
-        print('You guessed that already! Try again.')
-    
+# ask user if they'd like to play
+def gameTime():
+    print('Would you like to play hangman?')
+    ans = input()
+    if ans == 'yes':
+        playGame()
     else: 
+        print('Maybe next time!')
+        exit()
 
-        guesses.append(x)
+def playGame():
 
-        if len(guess) > 0:
-            print('Your guesses so far:', guesses)
-            for i in guess:
-                game_array[i] = x
-            game_display = ' '.join([str(char) for char in game_array])
-            print(game_display)
+    lines = open('./words.txt').readlines()
+    line = lines[0]
 
-        else:
-            print('That letter was wrong! Try again.')
-            count = count - 1
+    words = line.split(';')
+    comp_word = random.choice(words)
+    print(comp_word)
 
-else:
-    print('Game over! The phrase was', comp_word)
+    letters = list(comp_word)
 
+    game_array = []
 
+    for char in letters:
+        if char.isalpha():
+            game_array.append('_')
+        else: 
+            game_array.append(char)
 
+    # initial display, keeps it pretty for the game
+    game_display = ' '.join([str(char) for char in game_array])
+    print(game_display)
 
-# while count > 0:
-#     print('\nGuess a letter! You have', count, 'guesses left.')
-#     x = input()
-#     print(x, guess_array)
-#     if len(guess_array) == 0:
-#         guess_array.append(x)
-#     else: 
-#         if x: 
-#             guess_array.append(x)
-#             print(guess_array)
-#             count = count - 1
-#         else:
-#             print('You already guessed that letter!')
-#             print(guess_array)
+    count = 7
+    guesses = []
+
+    while count > 0:
+
+        print('\nGuess a letter! You have', count, 'lives left.')
+        x = input().upper()
+
+        if len(x) == 1 and x.isalpha():
+
+            guess = isGuessCorrect(comp_word.upper(), x)
+
+            if x in guesses:
+
+                print('You guessed that already! Try again.')
+            
+            else: 
+
+                guesses.append(x)
+
+                if len(guess) > 0:
+                    print('Your guesses so far:', guesses)
+                    for i in guess:
+                        game_array[i] = x
+                    game_display = ' '.join([str(char) for char in game_array])
+                    print(game_display)
+                    
+                    if isGameWon(game_display):
+                        print('Congrats! You won the game!')
+                        gameTime()
+
+                else:
+                    print('That letter was wrong! Try again.')
+                    count = count - 1
+        
+        else: 
+            print('Please choose just one letter at a time.')
+
+    else:
+        print('Game over! The phrase was', comp_word)
+        gameTime()
+
+gameTime()
